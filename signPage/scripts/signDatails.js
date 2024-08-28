eb345907c01392978c30df331edaf88b621fa6ac
Rupeshfuse <rupeshfuse77@gmail.com>
2024-08-28 19:06:38 +0530

    issue 28-08-2024

---
+++ signPage/scripts/signDatails.js
@@ -0,0 +1,29 @@
+import { url } from "./utility.js";
+
+// console.log("object");
+let getNum = localStorage.getItem("number");
+document.getElementById("number").value = getNum;
+
+document.getElementById("s_register_proceed").onclick = async () => {
+  let data = {
+    number: getNum,
+    full_name: document.getElementById("fullname").value,
+    email: document.getElementById("email").value,
+  };
+  let body = JSON.stringify(data);
+
+  await fetch(`${url}`, {
+    method: "GET",
+    body,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+document.getElementById("s_register_proceed").onclick = () => {
+  //location.href = "../index.html";
+  localStorage.setItem(
+    "full_name",
+    JSON.stringify(document.getElementById("fullname").value)
+  );
+};
